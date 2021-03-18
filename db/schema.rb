dd4c1c00ba75ff7c7377f3149e5bba34c24a5637
Yaroslav Kuzyo <yaroslav@jetruby.com>
2021-03-18 17:47:02 +0200

    Added skill model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_18_113629) do
+ActiveRecord::Schema.define(version: 2021_03_18_153641) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
