012b2717033ab881e752a6737739fdcc23dce351
Yaroslav Kuzyo <yaroslav@jetruby.com>
2021-03-20 07:44:14 +0200

    Added edit functionlity to portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -19,6 +19,21 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit
+    @portfolio = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: "Portfolio was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
     def portfolio_params
       params.require(:portfolio).permit(:title, :subtitle, :body)
