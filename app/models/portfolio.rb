b6cd3f97318bffc0e102e6be5f01ae37a34d70e8
Yaroslav Kuzyo <yaroslav@jetruby.com>
2021-03-26 15:54:37 +0200

    Added scope

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,8 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  def self.rails_items
+    where(subtitle: "Ruby on Rails")
+  end
+  scope :angular_items, -> {where(subtitle: "Angular")}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic ##{topic}",
+  )
+end
+
+puts "Topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog post ##{blog}",
-    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est recusandae excepturi eum sit illum minus? Blanditiis, ad voluptatem accusamus odio reiciendis autem eos distinctio soluta? Consequuntur itaque ipsa ad accusamus?"
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est recusandae excepturi eum sit illum minus? Blanditiis, ad voluptatem accusamus odio reiciendis autem eos distinctio soluta? Consequuntur itaque ipsa ad accusamus?",
+    topic_id: Topic.last.id
   )
 end
 
@@ -19,7 +28,16 @@ puts "Skills created"
 5.times do |portfolio|
   Portfolio.create!(
     title: "Portfolio ##{portfolio}",
-    subtitle: "Greate service",
+    subtitle: "Ruby on Rails",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est recusandae excepturi eum sit illum minus? Blanditiis, ad voluptatem accusamus odio reiciendis autem eos distinctio soluta? Consequuntur itaque ipsa ad accusamus?",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200",
+  )
+end
+1.times do |portfolio|
+  Portfolio.create!(
+    title: "Portfolio ##{portfolio}",
+    subtitle: "Angular",
     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est recusandae excepturi eum sit illum minus? Blanditiis, ad voluptatem accusamus odio reiciendis autem eos distinctio soluta? Consequuntur itaque ipsa ad accusamus?",
     main_image: "http://placehold.it/600x400",
     thumb_image: "http://placehold.it/350x200",
