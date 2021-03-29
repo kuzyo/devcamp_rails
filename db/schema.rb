de96dbbe43e5506b2849f2643e14c1080fd0886c
Yaroslav Kuzyo <yaroslav@jetruby.com>
2021-03-29 16:19:46 +0300

    Added placeholder concern

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_24_123615) do
+ActiveRecord::Schema.define(version: 2021_03_29_130641) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2021_03_24_123615) do
     t.integer "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|
