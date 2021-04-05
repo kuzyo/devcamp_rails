9351e2108a5105c31b6b1061a1489345b54b4705
Yaroslav Kuzyo <yaroslav@jetruby.com>
2021-04-05 14:13:57 +0300

    Added technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,3 +45,11 @@ end
 end
 
 puts "Portfolio items created"
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology ##{technology}",
+  )
+end
+
+puts "Technologies created"
