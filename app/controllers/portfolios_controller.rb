ae94bc06b55d4d10814e3aa7a1c5b0fab7a5374f
Yaroslav Kuzyo <yaroslav@jetruby.com>
2021-04-05 14:46:19 +0300

    Added nested attrs to portfolio form

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,6 +7,8 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio = Portfolio.new 
+
+    3.times {@portfolio.technologies.build}
   end
 
   def create
@@ -44,7 +46,7 @@ class PortfoliosController < ApplicationController
 
   private
     def portfolio_params
-      params.require(:portfolio).permit(:title, :subtitle, :body)
+      params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
     end
 
     def set_portfolio
